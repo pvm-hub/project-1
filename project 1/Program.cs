109e52c09fb465e5c50e8525cc5a83104cffe961
admin <admin@ADMIN-PC>
2022-09-30 20:27:37 +0200

    Добавьте файлы проекта.

---
+++ project 1/Program.cs
@@ -0,0 +1,13 @@
+﻿try
+{
+    Console.WriteLine("старт");
+    Console.WriteLine("введите x");
+    double x = double.Parse(Console.ReadLine());
+    double res = (x - (x * x * x) / 3 + (x * x * x * x *x) / 5);
+    Console.WriteLine(res);
+    Console.WriteLine("конец");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.ToString());
+}
